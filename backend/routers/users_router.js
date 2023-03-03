3786270a39c5b9ce44a3e9c3d8fdaebc51309b57
Youngjae Heo <youngjaeheo2002@gmail.com>
2023-03-02 23:03:11 -0500

    chore:run prettier

--- backend/routers/users_router.js
+++ backend/routers/users_router.js
@@ -9,29 +9,17 @@ export const usersRouter = Router();
 // TODO: add users routes
 
 //getting all
-usersRouter.get('/',async (req,res)=>{
-
-})
+usersRouter.get("/", async (req, res) => {});
 //getting one
-usersRouter.get('/:id',async (req,res)=>{
-
-})
+usersRouter.get("/:id", async (req, res) => {});
 
 //updating one
-usersRouter.patch("/:id",async(req,res)=>{
-
-})
+usersRouter.patch("/:id", async (req, res) => {});
 
 //deleting one
-usersRouter.delete("/:id",async (req,res)=>{
+usersRouter.delete("/:id", async (req, res) => {});
 
-})
-
-//signup 
-usersRouter.post("/signup", async (req, res) => {
-    
-});
+//signup
+usersRouter.post("/signup", async (req, res) => {});
 //signin
-usersRouter.post("/signin", async (req, res) => {
-    
-});
+usersRouter.post("/signin", async (req, res) => {});
