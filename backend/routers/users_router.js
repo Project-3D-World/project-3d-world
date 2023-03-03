5d50f449fd0ad0883c7152be40d631bf23ec76bb
Youngjae Heo <youngjaeheo2002@gmail.com>
2023-03-02 22:59:26 -0500

    feat: endpoints with no functionality for users

--- backend/routers/users_router.js
+++ backend/routers/users_router.js
@@ -7,3 +7,31 @@ const db = await getMongoSession();
 export const usersRouter = Router();
 
 // TODO: add users routes
+
+//getting all
+usersRouter.get('/',async (req,res)=>{
+
+})
+//getting one
+usersRouter.get('/:id',async (req,res)=>{
+
+})
+
+//updating one
+usersRouter.patch("/:id",async(req,res)=>{
+
+})
+
+//deleting one
+usersRouter.delete("/:id",async (req,res)=>{
+
+})
+
+//signup 
+usersRouter.post("/signup", async (req, res) => {
+    
+});
+//signin
+usersRouter.post("/signin", async (req, res) => {
+    
+});
