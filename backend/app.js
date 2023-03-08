0539b603eaa3d4fd0f90d237d7b5023caf94ad59
kypham <phamtuanky@gmail.com>
2023-03-07 23:45:33 -0500

    world-model: added endpoints for creating world and for users to claim chunks

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ import morgan from "morgan";
 import { openMongoSession, closeMongoSession } from "./datasource.js";
 
 import { usersRouter } from "./routers/users_router.js";
+import { worldsRouter } from "./routers/worlds_router.js";
 
 const port = 3000; // default port
 const app = express();
@@ -21,6 +22,7 @@ try {
 
 // TODO: add other routers
 app.use("/api/users", usersRouter);
+app.use("/api/worlds", worldsRouter);
 
 // start server
 const server = app.listen(port, () => {
