80edabf689fa4806ad08efec788853f9fcc7dc54
Youngjae Heo <youngjaeheo2002@gmail.com>
2023-03-07 23:20:16 -0500

    chore:run prettier

--- 3dworld/src/app/pages/sign-up/sign-up.component.ts
+++ 3dworld/src/app/pages/sign-up/sign-up.component.ts
@@ -6,14 +6,19 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
-  styleUrls: ['./sign-up.component.scss']
+  styleUrls: ['./sign-up.component.scss'],
 })
-export class SignUpComponent implements OnInit{
-  signUpStatus:string = `haven't tried yet`
+export class SignUpComponent implements OnInit {
+  signUpStatus: string = `haven't tried yet`;
   nameForm: FormGroup;
   @Output() newName = new EventEmitter<string>();
-  profileJson:string = '';
-  constructor(private fb: FormBuilder,private auth:AuthService,private api:ApiService,private router:Router) {
+  profileJson: string = '';
+  constructor(
+    private fb: FormBuilder,
+    private auth: AuthService,
+    private api: ApiService,
+    private router: Router
+  ) {
     this.nameForm = this.fb.group({
       /**
        * In Angular we can easily define a form field with validators, without installing 9 billion more
@@ -21,31 +26,29 @@ export class SignUpComponent implements OnInit{
        *
        * Add your fields here
        */
-      name:['',Validators.required],
+      name: ['', Validators.required],
     });
   }
 
   ngOnInit(): void {
     this.auth.user$.subscribe(
-      (profile)=>(this.profileJson = JSON.stringify(profile,null,2)
-      )
-    )
+      (profile) => (this.profileJson = JSON.stringify(profile, null, 2))
+    );
   }
 
   postMessage() {
     this.newName.emit(this.nameForm.value.name);
-    if(this.profileJson.length !== 0){
-      this.api.signUp(this.profileJson,this.nameForm.value.name).subscribe({
-        next:(x)=>{
-          this.router.navigateByUrl("");
-          this.signUpStatus = '200: you got signed in'
+    if (this.profileJson.length !== 0) {
+      this.api.signUp(this.profileJson, this.nameForm.value.name).subscribe({
+        next: (x) => {
+          this.router.navigateByUrl('');
+          this.signUpStatus = '200: you got signed in';
         },
-        error:(err)=>{
+        error: (err) => {
           console.log(err);
           this.signUpStatus = `${err.status} ${err.error.error}`;
-        }
+        },
       });
     }
   }
-
 }
--- 3dworld/src/app/services/api.service.ts
+++ 3dworld/src/app/services/api.service.ts
@@ -1,43 +1,36 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
-import { Observable,Subscription } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 @Injectable({
   providedIn: 'root',
 })
 export class ApiService {
   endpoint = environment.apiEndpoint;
-  constructor(private http:HttpClient) {}
-  
-  signIn(profile:string){
+  constructor(private http: HttpClient) {}
+
+  signIn(profile: string) {
     console.log(profile);
     const profileJson = JSON.parse(profile);
-    if(profileJson?.sub){
-      return this.http.post(this.endpoint+'/api/users/signin',{
-        sub:profileJson.sub
-      })
-    }
-    else{
+    if (profileJson?.sub) {
+      return this.http.post(this.endpoint + '/api/users/signin', {
+        sub: profileJson.sub,
+      });
+    } else {
       return new Observable();
-
     }
-    
-
   }
 
-  signUp(profile:string,displayName:string){
-    console.log(profile,displayName);
+  signUp(profile: string, displayName: string) {
+    console.log(profile, displayName);
     const profileJson = JSON.parse(profile);
-    if(profileJson?.sub){
-      return this.http.post(this.endpoint+'/api/users/signup',{
-        sub:profileJson.sub,
-        displayName:displayName
-      })
-    }
-    else{
-      return new Observable(
-
-      );
+    if (profileJson?.sub) {
+      return this.http.post(this.endpoint + '/api/users/signup', {
+        sub: profileJson.sub,
+        displayName: displayName,
+      });
+    } else {
+      return new Observable();
     }
   }
 }
