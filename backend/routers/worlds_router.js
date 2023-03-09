79ce600ed10fc01bd5bccabae2c85168f6d92f82
kypham <phamtuanky@gmail.com>
2023-03-08 20:36:20 -0500

    upload-chunk: added endpoint to upload a chunk

--- backend/routers/worlds_router.js
+++ backend/routers/worlds_router.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import multer from "multer";
+import fs from "fs";
 
 import { GridFile } from "../models/gridfiles.js";
 import { World } from "../models/worlds.js";
@@ -113,4 +114,56 @@ worldsRouter.patch("/:worldId/chunks/:chunkId", async (req, res) => {
 /* PUT /api/worlds/:worldId/chunks/:chunkId/file */
 worldsRouter.put("/:worldId/chunks/:chunkId/file", upload.single("chunkFile"),  async (req, res) => {
   /* an endpoint for user to upload a chunk file */
+  const { worldId, chunkId } = req.params;
+  const { userId } = req.body;
+  const chunkFile = req.file;
+  const user = await User.findById(userId);
+  if (!user) {
+    res.status(404).json({ error: "User not found" });
+    return;
+  }
+  const world = await World.findById(worldId);
+  if (!world) {
+    res.status(404).json({ error: "World not found" });
+    fs.unlinkSync(chunkFile.path);
+    return;
+  }
+  const chunk = world.chunks.id(chunkId);
+  if (!chunk) {
+    res.status(404).json({ error: "Chunk not found" });
+    fs.unlinkSync(chunkFile.path);
+    return;
+  }
+  if (chunk.claimedBy.toString() !== userId) {
+    res.status(403).json({ error: `This chunk is not claimed by the user ${userId}` });
+    fs.unlinkSync(chunkFile.path);
+    return;
+  }
+
+  // Upload the file to MongoDB and delete from /uploads
+  const fileStream = fs.createReadStream(chunkFile.path);
+  const gridFile = new GridFile({
+    filename: chunkFile.originalname,
+    contentType: chunkFile.mimetype,
+    metadata: {
+      world: worldId,
+      chunk: chunkId,
+    },
+  });
+  const uploadedChunk = await gridFile.upload(fileStream);
+  fs.unlinkSync(chunkFile.path);
+
+  // If there is already a file, delete it
+  if (chunk.file !== null) {
+    const oldFile = await GridFile.findById(chunk.chunkFile);
+    if (oldFile) {
+      await GridFile.deleteOne({ _id: oldFile._id });
+    }
+  }
+  chunk.chunkFile = uploadedChunk.id;
+  await world.save();
+  res.json({ 
+    message: "Chunk file uploaded",
+    chunk: chunk,
+  });
 });
