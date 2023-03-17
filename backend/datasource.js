a9fdea9cdedb7c82b2eee464f6ed40359a738ea3
kypham <phamtuanky@gmail.com>
2023-03-17 17:10:09 -0400

    tr-world: adopted sharedb and express-ws

--- backend/datasource.js
+++ backend/datasource.js
@@ -1,12 +1,14 @@
 import mongoose from "mongoose";
+import ShareDB from "sharedb";
+import ShareDbMongo from "sharedb-mongo";
 
 import { config } from "./config.js";
 
 const mongoURI = config.mongoURI;
 const mongoDbName = config.mongoDbName;
 
-let mongo_client = null; // this client will be used for sharedb later
-let gfs = null;
+let mongo_client = null;
+let shareBackend = null; 
 
 export const openMongoSession = async function () {
   if (!mongo_client) {
@@ -19,14 +21,23 @@ export const openMongoSession = async function () {
     console.log("Connected to MongoDB");
     mongoose.connection.once("open", () => {
       mongo_client = mongoose.connection.client;
-      gfs = Grid(conn.db, mongoose.mongo);
+      // Create ShareDB backend
+      if (!shareBackend) {
+        const db = new ShareDbMongo({ mongo: () => mongo_client });
+        shareBackend = new ShareDB({ db });
+      }
     });
   }
 };
 
 export const closeMongoSession = async function () {
   if (mongo_client) {
+    await shareBackend.close();
     await mongoose.disconnect();
     mongo_client = null;
   }
 };
+
+export const getShareBackend = function () {
+  return shareBackend;
+}
