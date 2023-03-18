fd42c89167184c91d5d4aecf1fbf5a33c6e33349
Youngjae Heo <youngjaeheo2002@gmail.com>
2023-03-17 21:05:41 -0400

    feat:auth0 guard

--- 3dworld/src/app/guards/auth0.guard.ts
+++ 3dworld/src/app/guards/auth0.guard.ts
@@ -1,15 +1,19 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { AuthService } from '@auth0/auth0-angular';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthguardGuard implements CanActivate {
+export class Auth0Guard implements CanActivate {
+  constructor(public auth:AuthService){
+
+  }
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    return this.auth.isAuthenticated$;
   }
   
 }
