ee00ab9eee2a9ac0b2337a43ed4b3670f9e4f915
Youngjae Heo <youngjaeheo2002@gmail.com>
2023-03-20 20:51:18 -0400

    Revert "Merge pull request #19 from UofT-UTSC-CS-sandbox/threejs-object"
    This reverts commit cd2f6cbd4e6fbc2da94ea9dc8b8c6fd95875dd04, reversing
    changes made to 492cf0a26d6ddad49fb4ea0c09b21bb8bc2365bf.

--- backend/app.js
+++ backend/app.js
@@ -9,6 +9,7 @@ import { config } from "./config.js";
 
 import { usersRouter } from "./routers/users_router.js";
 import { worldsRouter } from "./routers/worlds_router.js";
+import { commentsRouter } from "./routers/comments_router.js";
 
 const port = config.port;
 const app = express();
@@ -42,7 +43,7 @@ try {
 // TODO: add other routers
 app.use("/api/users", usersRouter);
 app.use("/api/worlds", worldsRouter);
-
+app.use( '/api/comments',commentsRouter)
 // start server
 const server = app.listen(port, () => {
   console.log(`Server started at http://localhost:${port}`);
