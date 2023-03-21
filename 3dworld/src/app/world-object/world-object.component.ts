6d1dfd408e7f9457b7590753e7af39c00db16502
JuefeiLu <juefeilu@gmail.com>
2023-03-21 14:28:59 -0400

    Modularize WorldObject

--- 3dworld/src/app/world-object/world-object.component.ts
+++ 3dworld/src/app/world-object/world-object.component.ts
@@ -14,13 +14,19 @@ export class WorldObjectComponent implements OnInit {
   camera!: THREE.PerspectiveCamera;
   renderer!: THREE.WebGLRenderer;
   worldId!: string;
+  raycaster!:THREE.Raycaster;
+  mouse!:THREE.Vector2;
+  models!: THREE.Group[];
+  ambientLight!: THREE.AmbientLight;
+  controls!: OrbitControls;
+  loader!: GLTFLoader;
 
   constructor(private api: ApiService) {
 
   }
 
   loadWorld(): void {
-    this.api.getWorld("64163ea8dcde7c82cc71f65e").subscribe((data) => {
+    this.api.getWorld('64176688914a579ebfb79af5').subscribe((data) => {
       console.log(data);
     });
   }
@@ -43,57 +49,35 @@ export class WorldObjectComponent implements OnInit {
     });
   }
 
-  ngOnInit() {
-    //get a world id 
-    // load the world with its chunks
-    // check if user clicks on a chunk
-    // if user clicks on a chunk, claim the chunk
-    // if user clicks on a chunk that is already claimed, show the owner of the chunk
-    // if user clicks on a chunk that is owned by the user, open a form to submit a new gltf model
-    this.loadWorld();
+  onWindowResize(){
+    this.camera.aspect = window.innerWidth / window.innerHeight;
+    this.camera.updateProjectionMatrix();
+    this.renderer.setSize( window.innerWidth, window.innerHeight );
+  }
 
-    this.api.getWorld("64163ea8dcde7c82cc71f65e").subscribe((data) => {
-    
-      let id = (<any>data).world._id;
-      let chunks = (<any>data).world.chunks;
-      let sidelength = Math.sqrt(chunks.length);
-      let chunkSize = (<any>data).world.chunkSize.x;
-      console.log(id);
-      console.log(chunks);
-      console.log(sidelength);
-      console.log(chunkSize);
-
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(
-      75,
-      window.innerWidth / window.innerHeight,
-      0.1,
-      1000
-    );
-    const renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(renderer.domElement);
-    const controls = new OrbitControls(camera, renderer.domElement);
-    const loader = new GLTFLoader();
-    const api = this.api;
-    const gridSize = 3;
+  loadChunks()
+  {
+    this.api.getWorld('64176688914a579ebfb79af5').subscribe((data) => {
+    let id = (<any>data).world._id;
+    let chunks = (<any>data).world.chunks;
+    let sidelength = Math.sqrt(chunks.length);
+    let chunkSize = (<any>data).world.chunkSize.x;
     const cubeSize = 10;
-    // add event listeners to the cube
-    const raycaster = new THREE.Raycaster();
-    const mouse = new THREE.Vector2();
-
-    const models:THREE.Group[] = [];
+    console.log(id);
+    console.log(chunks);
+    console.log(sidelength);
+    console.log(chunkSize);
 
     for (let x = 0; x < sidelength; x++) {
       for (let z = 0; z < sidelength; z++) {
         if((<any>data).world.chunks[x+z].chunkFile != null) {
-        loader.load(
+        this.loader.load(
           (<any>data).world.chunks[x+z].chunkFile,
-          function (gltf) {
+           (gltf) => {
             gltf.scene.scale.set(5, 5, 5);
             gltf.scene.position.set(x * cubeSize, 0, z*cubeSize);
-            models.push(gltf.scene);
-            scene.add(gltf.scene);
+            this.models.push(gltf.scene);
+            this.scene.add(gltf.scene);
           },
           function ( xhr ) {
             console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
@@ -109,75 +93,78 @@ export class WorldObjectComponent implements OnInit {
           const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
           const cube = new THREE.Mesh(geometry, material);
           cube.position.set(x * (chunkSize+1), 0, z * (chunkSize+1));
-          
           let group = new THREE.Group();
           group.add(cube);
-          scene.add(group);
-          models.push(group); // add each cube to the array
+          this.scene.add(group);
+          this.models.push(group); // add each cube to the array
         }
     }
+    }
+  });
   }
 
-    const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
-    scene.add(ambientLight);
-
-    camera.position.y = 10;
-    camera.position.z = 10;
-    camera.position.x = 10;
-    // add a click listener to the renderer's dom element
-    renderer.domElement.addEventListener('click', onClick, false);
-
-    window.addEventListener( 'resize', onWindowResize, false );
-
-    function onWindowResize(){
-
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-
-    renderer.setSize( window.innerWidth, window.innerHeight );
-
-    }
+  ngOnInit() {
+    //get a world id 
+    // load the world with its chunks
+    // check if user clicks on a chunk
+    // if user clicks on a chunk, claim the chunk
+    // if user clicks on a chunk that is already claimed, show the owner of the chunk
+    // if user clicks on a chunk that is owned by the user, open a form to submit a new gltf model
+    this.scene = new THREE.Scene();
+    this.camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    );
+    this.renderer = new THREE.WebGLRenderer();
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(this.renderer.domElement);
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+    this.loader = new GLTFLoader();
+    this.raycaster = new THREE.Raycaster();
+    this.mouse = new THREE.Vector2();
+    this.models = [];
+    this.ambientLight = new THREE.AmbientLight(0x404040);
+    // add event listeners to the cube
+    this.scene.add(this.ambientLight);
+    this.camera.position.y = 10;
+    this.camera.position.z = 10;
+    this.camera.position.x = 10;
+    this.loadChunks();
     
-    function onClick(event: MouseEvent) {
+    const onClick = (event: MouseEvent) => {
       // calculate mouse position in normalized device coordinates
       // (-1 to +1) for both components
-      mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-      mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+      console.log("mouseClick");
+      this.mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+      this.mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
       // update the picking ray with the camera and mouse position
-      raycaster.setFromCamera(mouse, camera);
+      this.raycaster.setFromCamera(this.mouse, this.camera);
       // calculate objects intersecting the picking ray
-      const intersects = raycaster.intersectObjects(models, true);
-
+      const intersects = this.raycaster.intersectObjects(this.models, true);
+  
       for (let i = 0; i < intersects.length; i++) {
         // perform the desired action on the clicked cube(s)
         //claim the cube by sending an api request to the server
         const model = intersects[i].object;
         console.log(model);
         console.log(model.getWorldPosition(new THREE.Vector3()));
-
-        // get world id
-        // get chunk id
-
-        // create the popup with world and chunk id if chunk is not claimed
-        /*var popup = document.createElement('div');
-        popup.className = 'popup';
-        popup.innerHTML = 'Claim this plot as your own?<br><button onclick="this.claimPlot()">Yes</button>';
-        document.body.appendChild(popup);
-        */
         break;
       }
     }
+    this.renderer.domElement.addEventListener('click', onClick, false);
+    window.addEventListener( 'resize', this.onWindowResize, false );    
 
-    function animate() {
-      controls.update();
-      raycaster.setFromCamera(mouse, camera);
-      renderer.clear();
-      renderer.render(scene, camera);
+    const animate = () => {
+      this.controls.update();
+      this.raycaster.setFromCamera(this.mouse, this.camera);
+      this.renderer.clear();
+      this.renderer.render(this.scene, this.camera);
       requestAnimationFrame(animate);
     }
-    
+
     requestAnimationFrame(animate);
-    });
+
   }
-  
 }
