9af1951f044821daefa4e16c73bb5b6bf84f0ab5
kypham <phamtuanky@gmail.com>
2023-03-25 17:57:46 -0400

    live-service: added function comments and minor features.

--- 3dworld/src/app/services/liveworld.service.ts
+++ 3dworld/src/app/services/liveworld.service.ts
@@ -1,36 +1,56 @@
 import { Injectable } from '@angular/core';
 
+/**
+ * Example world data:
+ * {
+ *  chunks: [
+ *   {
+ *     _id: 'chunkid',
+ *     upvotes: 0,
+ *     downvotes: 0,
+ *     chunkFile: 'fileid',
+ *     claimedBy: 'userid'
+ *   },
+ *   ...
+ *  ]
+ * }
+ */
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LiveWorldService {
   static Reaction = {
     upvote: 'upvote',
-    downvote: 'downvote'
-  }
+    downvote: 'downvote',
+  };
   private sharedb;
   private worldDoc: any;
   private socket: any;
   private opQueue: any[] = [];
-  
-  constructor() { 
+
+  constructor() {
     this.sharedb = require('sharedb/lib/client');
   }
 
   async connect(url: string, worldId: string) {
+    /**
+     * Connect to the live world at url and id. Returns a promise that resolves
+     * when the connection is established.
+     */
     if (this.socket) {
       this.socket.close();
     }
     this.socket = new WebSocket(url);
     const connection = new this.sharedb.Connection(this.socket);
     this.worldDoc = connection.get('worlds', worldId);
-    
+
     return new Promise<void>((resolve, reject) => {
       this.worldDoc.subscribe((err: any) => {
         if (err) {
           reject(err);
         } else {
-          this.opQueue.forEach(op => {
+          this.opQueue.forEach((op) => {
             this.worldDoc.submitOp(op);
           });
           this.opQueue = [];
@@ -48,6 +68,9 @@ export class LiveWorldService {
   }
 
   getWorldData() {
+    /**
+     * Returns the current world data. Returns null if the world is not connected.
+     */
     if (!this.worldDoc) {
       return null;
     }
@@ -55,10 +78,16 @@ export class LiveWorldService {
   }
 
   addReaction(chunkId: string, reaction: any) {
+    /**
+     * Adds a reaction to the chunk with the given id. The reaction must be one of
+     * the values in LiveWorldService.Reaction.
+     */
     if (reaction !== 'upvote' && reaction !== 'downvote') {
       return;
     }
-    const chunkIndex = this.worldDoc.data.chunks.findIndex((chunk: any) => chunk._id === chunkId);
+    const chunkIndex = this.worldDoc.data.chunks.findIndex(
+      (chunk: any) => chunk._id === chunkId
+    );
     if (chunkIndex === -1) {
       return;
     }
@@ -74,6 +103,11 @@ export class LiveWorldService {
   }
 
   onWorldChange(callback: any) {
+    /**
+     * Registers a callback that is called whenever the world data changes.
+     * The callback is passed the `op` that was applied to the world.
+     * ie: callback({ p: ['chunks', 0, 'upvotes'], oi: 1 })
+     */
     if (!this.worldDoc) {
       return;
     }
@@ -81,5 +115,4 @@ export class LiveWorldService {
       callback(op);
     });
   }
-
 }
