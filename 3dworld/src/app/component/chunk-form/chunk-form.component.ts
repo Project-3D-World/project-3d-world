27ae2aab97c7520c9905014728c6ca6ea0f63192
Youngjae Heo <youngjaeheo2002@gmail.com>
2023-03-28 23:27:58 -0400

    chore:run rpettier

--- 3dworld/src/app/component/chunk-form/chunk-form.component.ts
+++ 3dworld/src/app/component/chunk-form/chunk-form.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-chunk-form',
   templateUrl: './chunk-form.component.html',
-  styleUrls: ['./chunk-form.component.scss']
+  styleUrls: ['./chunk-form.component.scss'],
 })
-export class ChunkFormComponent {
-
-}
+export class ChunkFormComponent {}
