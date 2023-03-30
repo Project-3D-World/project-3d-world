95c92f37243a73a0e98c8f066a1e358076128e81
kypham <phamtuanky@gmail.com>
2023-03-29 22:47:59 -0400

    backend: added 500 error hanlder and added some world model changes

--- backend/models/worlds.js
+++ backend/models/worlds.js
@@ -11,31 +11,36 @@ const chunkSchema = new mongoose.Schema({
   claimedBy: { type: mongoose.ObjectId, default: null },
 });
 
-const chunksValidator = function (chunks) {
-  // the chunks must be unique by their location
-  const locations = new Set();
-  for (const c of chunks) {
-    if (locations.has(c.location)) {
-      return false;
-    }
-    locations.add(c.location);
-  }
-  return true;
-};
-
 const worldSchema = new mongoose.Schema({
   name: { type: String, required: true },
   chunkSize: {
     x: { type: Number, required: true },
-    y: { type: Number, required: true },
-    z: { type: Number, required: true },
   },
   description: String,
   rules: String,
   chunks: {
     type: [chunkSchema],
-    validate: [chunksValidator, "Duplicate chunk locations"],
   },
+});
+
+worldSchema.pre("validate", function (next) {
+  // chunks must not overlap
+  const chunkSize = this.chunkSize.x;
+  const chunks = this.chunks;
+  for (let i = 0; i < chunks.length; i++) {
+    const c1 = chunks[i];
+    for (let j = i + 1; j < chunks.length; j++) {
+      const c2 = chunks[j];
+      if (
+        Math.abs(c1.location.x - c2.location.x) < chunkSize &&
+        Math.abs(c1.location.z - c2.location.z) < chunkSize
+      ) {
+        next(new Error("Chunks overlap"));
+        return;
+      }
+    }
+  }
+  next();
 });
 
 export const World = mongoose.model("World", worldSchema);
