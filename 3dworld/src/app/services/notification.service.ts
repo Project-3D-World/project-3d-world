b19c055899a0fe5093b0721306baecb1456d9ebe
kypham <phamtuanky@gmail.com>
2023-03-31 10:34:19 -0400

    notif-backend: added angular module for socketio

--- 3dworld/src/app/services/notification.service.ts
+++ 3dworld/src/app/services/notification.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { Socket } from 'ngx-socket-io';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class NotificationService {
 
-  constructor() { }
+  constructor(private socketio: Socket) { }
+
+  sendNotification(notification: any) {
+    // check for required fields
+    if (!notification?.receiver ||
+      !notification?.rating ||
+      !notification?.worldName ||
+      !notification?.worldId ) {
+        console.log("NotificationService: sendNotification: missing required fields");
+        return;  
+    }
+    this.socketio.emit('notification', notification);
+  }
+
+  getNotifications() {
+    return this.socketio.fromEvent('notification');
+  }
 }
