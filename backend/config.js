dce9a5b7fac5d83dc01aef2263a6e99d960cd57c
kypham <phamtuanky@gmail.com>
2023-04-03 16:07:12 -0400

    backend-deployment: added Dockerfile for backend and removed PORT env to use default port 3000

--- backend/config.js
+++ backend/config.js
@@ -7,7 +7,6 @@ export const config = {
   mongoURI: process.env.MONGO_URI,
   mongoDbName: process.env.MONGO_DB_NAME,
   frontendBaseUrl: process.env.FRONTEND_BASE_URL,
-  port: process.env.PORT,
   sessionSecret: process.env.SESSION_SECRET,
   sendGrid_Api_key: process.env.SENDGRID_API_KEY,
 };
