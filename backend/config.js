3822c45324b6fcb4770d8ddcdc63ffd87a15fa2f
kypham <phamtuanky@gmail.com>
2023-04-03 18:31:21 -0400

    backend-deployment: added redis uri to the backend environment

--- backend/config.js
+++ backend/config.js
@@ -6,6 +6,7 @@ dotenv.config();
 export const config = {
   mongoURI: process.env.MONGO_URI,
   mongoDbName: process.env.MONGO_DB_NAME,
+  redisURI: process.env.REDIS_URI,
   frontendBaseUrl: process.env.FRONTEND_BASE_URL,
   sessionSecret: process.env.SESSION_SECRET,
   sendGrid_Api_key: process.env.SENDGRID_API_KEY,
