5619f8e60fb03b79cfe6ce618325de25e22d1d86
kypham <phamtuanky@gmail.com>
2023-04-03 21:38:38 -0400

    backend-deployment: added adminInit to create an admin account from given sub.

---
+++ backend/adminInit.js
@@ -0,0 +1,25 @@
+import { User } from "./models/users.js";
+import { config } from "./config.js"
+
+const adminSub = config.adminSub;
+
+export const initAdmin = async function () {
+  const existingAdmin = await User.findOne({ sub: adminSub });
+  if (!existingAdmin) {
+    const admin = new User({
+      sub: adminSub,
+      displayName: "admin",
+      email: "admin@localhost.loc",
+      claims: [],
+      isAdmin: true,
+    });
+    await admin.save();
+  } else {
+    existingAdmin.sub = adminSub;
+    existingAdmin.displayName = "admin";
+    existingAdmin.email = "admin@localhost.loc";
+    existingAdmin.claims = [];
+    existingAdmin.isAdmin = true;
+    await existingAdmin.save();
+  }
+};
